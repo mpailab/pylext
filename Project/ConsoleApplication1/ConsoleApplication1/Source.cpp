8a3c91e91a25ae33d2f1c506289f9b4add0afed5
Broshan <gosha-saf@yandex.ru>
2021-03-11 16:16:03 +0300

    correct test bad worcking

--- Project/ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ Project/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,9 +1,11 @@
 #include "Structure.h"
 int test1();
 int test2();
+void correct_test();
 int main()
 {
 	int n;
+	//correct_test();
 	cout << "What test did you want? 1 - value; 2 - return_value" << endl;
 	cin >> n;
 	if (n == 1)
