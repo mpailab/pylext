f55e9c54f71a0ed18711827d6f41d6ed5a91c6fe
Broshan <gosha-saf@yandex.ru>
2021-03-17 13:01:02 +0300

    Добавил дек массивов и его speed_test

--- data_structures/position_map/PositionMap.h
+++ data_structures/position_map/PositionMap.h
@@ -1,6 +1,9 @@
 #include <unordered_map>
+#include <deque>  
+#include <array>
 #include <stdexcept>
 #include <concepts>
+#include <string>
 #include "Common.h"
 
 class AssertionFailed: public std::runtime_error {
@@ -9,7 +12,7 @@ public:
 };
 
 #ifdef _DEBUG
-#define ASSERT(x) { if(!(x)) throw AssertionFailed(#x __FILE__ __LINE__); }
+#define ASSERT(x) { if(!(x)) throw AssertionFailed("Assertion " #x " failed at " _FILE_ ", line "+std::to_string(__LINE__)); }
 #else
 #define ASSERT(x)
 #endif
@@ -63,4 +66,43 @@ public:
 
     // Функция сообщает структуре данных, что можно стереть все ключи (p, nt) с p < pos
     void erase_before(int pos){ erased = std::max(pos, erased); }
+};
+
+
+template<class V>
+class PositionMapDA {
+    int erased = 0;
+    V _default;
+    std::deque<array<V,1024>> _data;
+public:
+    explicit PositionMapDA(V default_val = {}) : _default(default_val) {}
+    // Ищет в структуре данных ключ (pos, nt) и возвращает 0, если не найдено и указатель на значение, если надено
+    const V* find(int pos, int nt) const {
+        ASSERT(pos>=erased);
+        int len = int(_data.size() - 1);
+        return ((pos - erased) > len) ? nullptr : &(_data[pos - erased][nt]);
+        //auto it = _data.find(std::make_pair(pos, nt));
+        //return (it == _data.end()) ? nullptr : &it->second;
+    }
+    // Делает то же самое, но если ключ (pos, nt) не найден, то добавляется значение V{} с таким ключом и возвращается ссылка на него
+    V& operator()(int pos, int nt) {
+        ASSERT(pos>=erased);
+        int len = int(_data.size() - 1);
+        if ((pos - erased) > len ) {
+            std::array<V, 1024> _buf;
+            _buf.fill(_default);
+            int nb = (pos - erased) - len;
+            for (int i = 0; i < nb; i++)
+                _data.push_back(_buf);
+        }
+        return _data[(pos - erased)][nt];
+        //return _data.insert(std::make_pair(std::make_pair(pos, nt), _default)).first->second;
+    }
+
+    // Функция сообщает структуре данных, что можно стереть все ключи (p, nt) с p < pos
+    void erase_before(int pos) {
+        for (int i = 0; i < pos - erased; i++)
+            _data.pop_back();
+        erased = std::max(pos, erased); 
+    }
 };
